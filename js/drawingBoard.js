bdf7881fcc0bbe49d0469da5db76b2d6116a3bbc
Fox Tail <foxtail25@gmail.com>
2025-02-28 17:21:10 +0300

    Удалил пустые строки. Добавил описание проекта.

--- js/drawingBoard.js
+++ js/drawingBoard.js
@@ -1,7 +1,5 @@
 const canvas = document.getElementById("canvas-draw-board");
 const context = canvas.getContext("2d");
-
-
 // установка цвета
 let colorInput = document.getElementById('colorInput')
 let canvasColor = colorInput.value;
@@ -9,41 +7,30 @@ colorInput.addEventListener('change', () => {
 	context.strokeStyle = colorInput.value;
 	// console.log(colorInput.value)
 })
-
 // установка фона
 context.fillRect(0, 0, canvas.width, canvas.height);
 context.fillStyle = '#FFFFFF';
 context.fillRect(0, 0, canvas.width, canvas.height);
 context.strokeStyle = canvasColor;
-
 // очищение canvas
-document.getElementById('clearCanvasBtn').addEventListener('click', ()=> context.clearRect(0,0,canvas.width, canvas.height));
-
+document.getElementById('clearCanvasBtn').addEventListener('click', () => context.clearRect(0, 0, canvas.width, canvas.height));
 //установка толщины линии
 const lineWeight = document.getElementById('brushWeight');
 lineWeight.addEventListener('change', () => context.lineWidth = lineWeight.value);
-
 //установка размытия линии
 const lineblur = document.getElementById('brushBlur');
 lineWeight.addEventListener('change', () => context.filter = `blur(${lineblur.value}px)`);
 context.filter = `blur(${lineblur.value}px)`;
 lineblur.addEventListener('change', () => context.filter = `blur(${lineblur.value}px)`)
-
 // console.log(context)
-
 const w = canvas.width;
 const h = canvas.height;
-
 const mouse = { x: 0, y: 0 };      // координаты мыши
 let draw = false;
-
 //толщина линии
 context.lineWidth = lineWeight.value;
-
 // нажатие мыши
 canvas.addEventListener("mousedown", function (e) {
-	
-	
 	mouse.x = e.pageX - this.offsetLeft;
 	mouse.y = e.pageY - this.offsetTop;
 	draw = true;
@@ -52,21 +39,15 @@ canvas.addEventListener("mousedown", function (e) {
 });
 // перемещение мыши
 canvas.addEventListener("mousemove", function (e) {
-	
 	if (draw == true) {
-		
-		// context.filter = 'opacity(3%)';
-		// context.filter = 'opacity(30%)';
 		mouse.x = e.pageX - this.offsetLeft;
 		mouse.y = e.pageY - this.offsetTop;
 		context.lineTo(mouse.x, mouse.y);
 		context.stroke();
 	}
 });
-
 // отпускаем мышь
 canvas.addEventListener("mouseup", function (e) {
-	
 	mouse.x = e.pageX - this.offsetLeft;
 	mouse.y = e.pageY - this.offsetTop;
 	context.lineTo(mouse.x, mouse.y);
@@ -74,18 +55,12 @@ canvas.addEventListener("mouseup", function (e) {
 	context.closePath();
 	draw = false;
 });
-
-
 const saveBtn = document.getElementById('saveImgBtn');
 saveBtn.addEventListener('click', saveImgFunc);
-
-
 function saveImgFunc() {
 	const dataUrl = canvas.toDataURL('image/png');
 	const link = document.createElement('a');
 	link.href = dataUrl;
 	link.download = 'шедевр.png';
-
 	link.click();
-
 }
--- js/util/canvasWork.js
+++ js/util/canvasWork.js
@@ -14,25 +14,15 @@ class CanvasW {
     setCircleRadius(radius){
         this.circleRadius = radius;
     }
-
-    _getRadians(degrees) {
-        return (Math.PI / 180) * degrees;
-    }
-    _setCanvasBackground(){
-        console.log('setCanvasBg');
-        this.ctx.fillStyle = '#FFFFFF';
-        this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
-        this.ctx.style = this.canvasColor;
-    }
-    paintCircle(x,y){
+    paintCircle(x, y) {
         let ctx = this.ctx;
         ctx.beginPath();
         ctx.arc(x, y, this.circleRadius, 0, this._getRadians(360));
         ctx.fillStyle = this.canvasColor;
-		ctx.fill();
+        ctx.fill();
         ctx.closePath();
     }
-    clearCanvas(){
+    clearCanvas() {
         console.log('clear');
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
         this._setCanvasBackground();
@@ -40,6 +30,16 @@ class CanvasW {
     drawImg(img) {
         this.ctx.drawImage(img, 10, 10)
     }
+    _setCanvasBackground(){
+        console.log('setCanvasBg');
+        this.ctx.fillStyle = '#FFFFFF';
+        this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
+        this.ctx.style = this.canvasColor;
+    }
+    _getRadians(degrees) {
+        return (Math.PI / 180) * degrees;
+    }
+  
 }
 
 export default CanvasW
