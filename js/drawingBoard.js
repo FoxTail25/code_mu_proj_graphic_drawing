25a8d4e1b23a42d4d0861eb126683a63eb6a7b2d
Fox Tail <foxtail25@gmail.com>
2025-03-01 17:12:55 +0300

    Анимация при стирании

--- js/drawingBoard.js
+++ js/drawingBoard.js
@@ -1,83 +1,177 @@
-const canvas = document.getElementById("canvas-draw-board");
-const context = canvas.getContext("2d");
+
+// const canvas = document.getElementById("canvas-draw-board");
+// const context = canvas.getContext("2d");
+
+
+// // установка цвета
+// let colorInput = document.getElementById('colorInput')
+// let canvasColor = colorInput.value;
+// colorInput.addEventListener('change', () => {
+// 	context.strokeStyle = colorInput.value;
+// 	console.log(colorInput.value)
+// })
+// // установка фона
+// function setCanvasBackground(){
+// 	context.fillRect(0, 0, canvas.width, canvas.height);
+// 	context.fillStyle = '#FFFFFF';
+// 	context.fillRect(0, 0, canvas.width, canvas.height);
+// 	context.strokeStyle = canvasColor;
+// }
+// setCanvasBackground()
+// // очищение canvas
+// document.getElementById('clearCanvasBtn').addEventListener('click', () => {context.clearRect(0, 0, canvas.width, canvas.height)
+
+// 	clearAnimation()
+
+// });
+// //установка толщины линии
+// const lineWeight = document.getElementById('brushWeight');
+// lineWeight.addEventListener('change', () => context.lineWidth = lineWeight.value);
+
+// //установка размытия линии
+// const lineblur = document.getElementById('brushBlur');
+// lineWeight.addEventListener('change', () => context.filter = `blur(${lineblur.value}px)`);
+// context.filter = `blur(${lineblur.value}px)`;
+// lineblur.addEventListener('change', () => context.filter = `blur(${lineblur.value}px)`);
+
+// const w = canvas.width;
+// const h = canvas.height;
+
+// const mouse = { x: 0, y: 0 };      // координаты мыши
+// let draw = false;
+// //толщина линии
+// context.lineWidth = lineWeight.value;
+// // нажатие мыши
+// canvas.addEventListener("mousedown", function (e) {
+// 	mouse.x = e.layerX - this.offsetLeft;
+// 	mouse.y = e.layerY - this.offsetTop;
+// 	// console.log(e)
+// 	draw = true;
+// 	context.beginPath();
+// 	context.moveTo(mouse.x, mouse.y);
+// });
+// // перемещение мыши
+// canvas.addEventListener("mousemove", function (e) {
+// 	if (draw == true) {
+// 		mouse.x = e.layerX - this.offsetLeft;
+// 		mouse.y = e.layerY - this.offsetTop;
+// 		context.lineTo(mouse.x, mouse.y);
+// 		context.stroke();
+// 	}
+// });
+// // отпускаем мышь
+// canvas.addEventListener("mouseup", function (e) {
+// 	mouse.x = e.layerX - this.offsetLeft;
+// 	mouse.y = e.layerY - this.offsetTop;
+// 	context.lineTo(mouse.x, mouse.y);
+// 	context.stroke();
+// 	context.closePath();
+// 	draw = false;
+// });
+// const saveBtn = document.getElementById('saveImgBtn');
+// saveBtn.addEventListener('click', saveImgFunc);
+// function saveImgFunc() {
+// 	const dataUrl = canvas.toDataURL('image/png');
+// 	const link = document.createElement('a');
+// 	link.href = dataUrl;
+// 	link.download = 'шедевр.png';
+// 	link.click();
+// }
+
+
+
+
+// // Анимация стирания
+// function clearAnimation(){
+// 	console.log('clear anim')
+// 	setCanvasBackground()
+// }
+
+
+
+
+
+import CanvasW from './util/canvasWork.js';
+
+let canvas = document.getElementById("canvas-draw-board");
+
+const canvasWork = new CanvasW(canvas);
+
+// установка bg white
+canvasWork.setCanvasBackground('white');
+// рисование линии
+canvas.addEventListener("mousedown", (e) => canvasWork.startBrushPaint(e));
+canvas.addEventListener("mousemove", (e) => canvasWork.brushPaint(e));
+canvas.addEventListener("mouseup", (e) => canvasWork.stopBrushPaint(e));
 // установка цвета
-let colorInput = document.getElementById('colorInput')
-let canvasColor = colorInput.value;
-colorInput.addEventListener('change', () => {
-	context.strokeStyle = colorInput.value;
-	console.log(colorInput.value)
-})
-// установка фона
-function setCanvasBackground(){
-	context.fillRect(0, 0, canvas.width, canvas.height);
-	context.fillStyle = '#FFFFFF';
-	context.fillRect(0, 0, canvas.width, canvas.height);
-	context.strokeStyle = canvasColor;
-}
-setCanvasBackground()
-// очищение canvas
-document.getElementById('clearCanvasBtn').addEventListener('click', () => {context.clearRect(0, 0, canvas.width, canvas.height)
-
-	clearAnimation()
-
-});
+document.getElementById('colorInput').addEventListener('change', (e) => { canvasWork.setColor(e.target.value) });
 //установка толщины линии
-const lineWeight = document.getElementById('brushWeight');
-lineWeight.addEventListener('change', () => context.lineWidth = lineWeight.value);
+document.getElementById('brushWeight').addEventListener('change', (e) => canvasWork.setBrushWeight(e.target.value));
 //установка размытия линии
-const lineblur = document.getElementById('brushBlur');
-lineWeight.addEventListener('change', () => context.filter = `blur(${lineblur.value}px)`);
-context.filter = `blur(${lineblur.value}px)`;
-lineblur.addEventListener('change', () => context.filter = `blur(${lineblur.value}px)`);
-const w = canvas.width;
-const h = canvas.height;
-const mouse = { x: 0, y: 0 };      // координаты мыши
-let draw = false;
-//толщина линии
-context.lineWidth = lineWeight.value;
-// нажатие мыши
-canvas.addEventListener("mousedown", function (e) {
-	mouse.x = e.layerX - this.offsetLeft;
-	mouse.y = e.layerY - this.offsetTop;
-	// console.log(e)
-	draw = true;
-	context.beginPath();
-	context.moveTo(mouse.x, mouse.y);
-});
-// перемещение мыши
-canvas.addEventListener("mousemove", function (e) {
-	if (draw == true) {
-		mouse.x = e.layerX - this.offsetLeft;
-		mouse.y = e.layerY - this.offsetTop;
-		context.lineTo(mouse.x, mouse.y);
-		context.stroke();
-	}
-});
-// отпускаем мышь
-canvas.addEventListener("mouseup", function (e) {
-	mouse.x = e.layerX - this.offsetLeft;
-	mouse.y = e.layerY - this.offsetTop;
-	context.lineTo(mouse.x, mouse.y);
-	context.stroke();
-	context.closePath();
-	draw = false;
-});
-const saveBtn = document.getElementById('saveImgBtn');
-saveBtn.addEventListener('click', saveImgFunc);
-function saveImgFunc() {
-	const dataUrl = canvas.toDataURL('image/png');
+document.getElementById('brushBlur').addEventListener('change', (e) => canvasWork.setBlur(e.target.value));
+// Кнопка сохранения рисунка
+document.getElementById('saveImgBtn').addEventListener('click', function saveImgFunc() {
+	const dataUrl = canvasWork.getCanvas().toDataURL('image/png');
 	const link = document.createElement('a');
 	link.href = dataUrl;
 	link.download = 'шедевр.png';
 	link.click();
-}
-
-
-
+})
+// Сохранение рисунка
+document.getElementById('saveImgBtn').addEventListener('click',
+	function saveImgFunc() {
+		const dataUrl = canvas.toDataURL('image/png');
+		const link = document.createElement('a');
+		link.href = dataUrl;
+		link.download = 'шедевр.png';
+		link.click();
+	}
+);
+// очищение canvas
+document.getElementById('clearCanvasBtn').addEventListener('click', () => {
+	clearAnimation()
+})
+let front = document.querySelector('.front');
+let back = document.querySelector('.back');
 
 // Анимация стирания
+//необходимая переменная для второй версии анимации
+let animationFlag = 1;
+function clearAnimation() {
 
-function clearAnimation(){
-	console.log('clear anim')
-	setCanvasBackground()
+	// Первая версия анимации
+
+	// console.log('clear anim')
+	// front.classList.add('rotate');
+	// back.classList.add('rotate');
+	// setTimeout(() => {
+	// 	canvasWork.clearCanvas()
+	// 	document.styleSheets[1].cssRules[4].styleSheet.rules[13].style.transition = '0s';
+	// 	front.classList.remove('rotate');
+	// 	back.classList.remove('rotate');
+	// }, 600)
+	// document.styleSheets[1].cssRules[4].styleSheet.rules[13].style.transition = '0.8s';
+
+	//Вторая версия анимации
+	if (animationFlag == 1) {
+		animationFlag = 2;
+		front.classList.add('rotate');
+		back.classList.add('rotate');
+		setTimeout(() => {
+			canvas = canvas.parentElement.removeChild(canvas);
+			back.appendChild(canvas);
+			canvasWork.clearCanvas();
+		}, 300)
+	} else {
+		animationFlag = 1;
+		front.classList.remove('rotate');
+		back.classList.remove('rotate');
+		setTimeout(() => {
+			canvas = canvas.parentElement.removeChild(canvas);
+			front.appendChild(canvas);
+			canvasWork.clearCanvas();
+		}, 300)
+	}
+
+
 }
--- js/util/canvasWork.js
+++ js/util/canvasWork.js
@@ -1,94 +1,62 @@
-
-// class CanvasW {
-//     constructor(canvasNodeElement) {
-//         this.canvas = canvasNodeElement;
-//         this.ctx = this.canvas.getContext('2d');
-//         this._setCanvasBackground();
-//     }
-//     canvasColor = '#000000';
-//     circleRadius = 1;
-
-//     setColor(color){
-//         this.canvasColor = color;
-//     }
-//     setCircleRadius(radius){
-//         this.circleRadius = radius;
-//     }
-//     paintCircle(x, y) {
-//         let ctx = this.ctx;
-//         ctx.beginPath();
-//         ctx.arc(x, y, this.circleRadius, 0, this._getRadians(360));
-//         ctx.fillStyle = this.canvasColor;
-//         ctx.fill();
-//         ctx.closePath();
-//     }
-//     clearCanvas() {
-//         console.log('clear');
-//         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
-//         this._setCanvasBackground();
-//     }
-//     drawImg(img) {
-//         this.ctx.drawImage(img, 10, 10)
-//     }
-//     _setCanvasBackground(){
-//         console.log('setCanvasBg');
-//         this.ctx.fillStyle = '#FFFFFF';
-//         this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
-//         this.ctx.style = this.canvasColor;
-//     }
-//     _getRadians(degrees) {
-//         return (Math.PI / 180) * degrees;
-//     }
-// }
-
-// export default CanvasW
-
-
-
 class CanvasW {
     constructor(canvasNodeElement) {
         this.canvas = canvasNodeElement;
         this.ctx = this.canvas.getContext('2d');
-        this._setCanvasBackground();
     }
-    canvasColor = '#000000';
-    brushWeight = 1;
-    brushBlur = 0;
-    draw = false;
-
+    mouse = { x: 0, y: 0 };
     setColor(color){
-        this.canvasColor = color;
+        this.ctx.strokeStyle = color;
     };
     setBrushWeight(num){
-        this.brushWeight = num;
+        this.ctx.lineWidth = num;
     };
     setBlur(num) {
-        this.brushBlur = num;
+        this.ctx.filter = `blur(${num}px)`;
+    }
+    setCanvasBackground(color){
+        let context = this.ctx;
+        if(color){
+        	context.fillStyle = color;
+        } else {
+            context.fillStyle = '#FFFFFF';
+        }
+        	context.fillRect(0, 0, this.canvas.width, this.canvas.height);
+    }
+    clearCanvas(){
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+        this.setCanvasBackground();
     }
     startBrushPaint(e){
-        mouse.x = e.layerX - this.offsetLeft;
-        // mouse.x = e.layerX - this.canvas.offsetLeft;
-        mouse.y = e.layerY - this.offsetTop;
-        // mouse.x = e.layerX - this.canvas.offsetLeft;
+        let mouse = this.mouse;
+        let context = this.ctx;
+        mouse.x = e.layerX - this.canvas.offsetLeft;
+        mouse.y = e.layerY - this.canvas.offsetTop;
         this.draw = true;
         context.beginPath();
         context.moveTo(mouse.x, mouse.y);
     }
     brushPaint(e) {
+        let mouse = this.mouse;
+        let context = this.ctx;
         if (this.draw == true) {
-            mouse.x = e.layerX - this.offsetLeft;
-            mouse.y = e.layerY - this.offsetTop;
+            mouse.x = e.layerX - this.canvas.offsetLeft;
+            mouse.y = e.layerY - this.canvas.offsetTop;
             context.lineTo(mouse.x, mouse.y);
             context.stroke();
         }
     }
     stopBrushPaint(e){
-        mouse.x = e.layerX - this.offsetLeft;
-        mouse.y = e.layerY - this.offsetTop;
+        let mouse = this.mouse;
+        let context = this.ctx;
+        mouse.x = e.layerX - this.canvas.offsetLeft;
+        mouse.y = e.layerY - this.canvas.offsetTop;
         context.lineTo(mouse.x, mouse.y);
         context.stroke();
         context.closePath();
-        draw = false;
+        this.draw = false;
+    }
+    getCanvas(){
+        return this.canvas;
     }
 }
 
