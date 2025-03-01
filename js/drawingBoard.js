e3cd7debb6b38e846b3f91a7184fa0cf4b878a5e
Fox Tail <foxtail25@gmail.com>
2025-03-01 12:41:17 +0300

    переделал на relativ и absolute

--- js/drawingBoard.js
+++ js/drawingBoard.js
@@ -13,7 +13,11 @@ context.fillStyle = '#FFFFFF';
 context.fillRect(0, 0, canvas.width, canvas.height);
 context.strokeStyle = canvasColor;
 // очищение canvas
-document.getElementById('clearCanvasBtn').addEventListener('click', () => context.clearRect(0, 0, canvas.width, canvas.height));
+document.getElementById('clearCanvasBtn').addEventListener('click', () => {context.clearRect(0, 0, canvas.width, canvas.height)
+
+	clearAnimation()
+
+});
 //установка толщины линии
 const lineWeight = document.getElementById('brushWeight');
 lineWeight.addEventListener('change', () => context.lineWidth = lineWeight.value);
@@ -30,8 +34,9 @@ let draw = false;
 context.lineWidth = lineWeight.value;
 // нажатие мыши
 canvas.addEventListener("mousedown", function (e) {
-	mouse.x = e.pageX - this.offsetLeft;
-	mouse.y = e.pageY - this.offsetTop;
+	mouse.x = e.layerX - this.offsetLeft;
+	mouse.y = e.layerY - this.offsetTop;
+	// console.log(e)
 	draw = true;
 	context.beginPath();
 	context.moveTo(mouse.x, mouse.y);
@@ -39,16 +44,16 @@ canvas.addEventListener("mousedown", function (e) {
 // перемещение мыши
 canvas.addEventListener("mousemove", function (e) {
 	if (draw == true) {
-		mouse.x = e.pageX - this.offsetLeft;
-		mouse.y = e.pageY - this.offsetTop;
+		mouse.x = e.layerX - this.offsetLeft;
+		mouse.y = e.layerY - this.offsetTop;
 		context.lineTo(mouse.x, mouse.y);
 		context.stroke();
 	}
 });
 // отпускаем мышь
 canvas.addEventListener("mouseup", function (e) {
-	mouse.x = e.pageX - this.offsetLeft;
-	mouse.y = e.pageY - this.offsetTop;
+	mouse.x = e.layerX - this.offsetLeft;
+	mouse.y = e.layerY - this.offsetTop;
 	context.lineTo(mouse.x, mouse.y);
 	context.stroke();
 	context.closePath();
@@ -62,4 +67,13 @@ function saveImgFunc() {
 	link.href = dataUrl;
 	link.download = 'шедевр.png';
 	link.click();
+}
+
+
+
+
+// Анимация стирания
+
+function clearAnimation(){
+	
 }
